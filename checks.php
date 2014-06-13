b9697fc3e9e884471aa6fb0d60dffa1c202eca84
VincentIII <vincentiii@gmail.com>
2014-06-13 11:08:18 -0400

    Delete Bug Fix
    Fixed SQL Query for Deleting Checks

--- checks.php
+++ checks.php
@@ -371,7 +371,7 @@
 				else if ($fAction == "Delete")
 				{
 					$total++;
-					$query = "DELETE FROM checks WHERE cId='$properKey' ";
+					$query = "DELETE FROM checks WHERE cId=?' ";
 					if ($stmt=$connection->prepare($query))
 					{
 						$stmt->bind_param("i",$properKey);
